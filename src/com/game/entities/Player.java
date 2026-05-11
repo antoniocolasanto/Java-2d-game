c122eece8656aa8755716a4142e833d3447ed22c
antoniocolasanto <antoniocolasanto23@gmail.com>
2026-05-11 19:13:21 +0200

    Creazione e commenti a tutte le classi

--- src/com/game/entities/Player.java
+++ src/com/game/entities/Player.java
@@ -1,48 +1,11 @@
 package com.game.entities;
 
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-
-public class Player extends Entity {
-    private BufferedImage image;
-    private boolean left, right, up, down; // Direzioni
-    private float playerSpeed = 4.0f;      // Velocità di movimento
-
-    public Player(float x, float y, int width, int height) {
-        super(x, y, width, height);
-        loadImage();
-    }
-
-    private void loadImage() {
-        try {
-            // Assicurati che il percorso combaci con la tua cartella res!
-            File file = new File("res/Sprites/Characters/Default/character_green_idle.png");
-            image = ImageIO.read(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("ERRORE: Impossibile trovare l'immagine del personaggio!");
-        }
-    }
-
-    public void update() {
-        // Aggiorna le coordinate se il tasto corrispondente è premuto
-        if (left) x -= playerSpeed;
-        if (right) x += playerSpeed;
-        if (up) y -= playerSpeed;
-        if (down) y += playerSpeed;
-    }
-
-    public void render(Graphics g) {
-        // Disegna l'immagine alle coordinate correnti
-        g.drawImage(image, (int) x, (int) y, width, height, null);
-    }
-
-    // Metodi Set per cambiare lo stato del movimento (usati dalla tastiera)
-    public void setLeft(boolean left) { this.left = left; }
-    public void setRight(boolean right) { this.right = right; }
-    public void setUp(boolean up) { this.up = up; }
-    public void setDown(boolean down) { this.down = down; }
+/**
+ * IL GIOCATORE PRINCIPALE (Estende Entity)
+ * - Contiene la logica di movimento orizzontale e la fisica del salto (gravità, ySpeed).
+ * - Carica la sua immagine o animazione (spritesheet).
+ * - Comunica con KeyInput per sapere se l'utente sta premendo i tasti direzionali o la barra spaziatrice.
+ */
+public class Player {
+    // ...
 }
---
+++ src/com/game/graphics/Renderer.java
@@ -0,0 +1,11 @@
+package com.game.graphics;
+
+/**
+ * MOTORE GRAFICO AUSILIARIO
+ * - Classe utility che aiuta il GamePanel a disegnare sprite complessi o UI.
+ * - Potrebbe gestire la Camera: se il player va a destra, sposta tutti gli oggetti
+ * disegnati a schermo verso sinistra per dare l'illusione del movimento.
+ */
+public class Renderer {
+    // ...
+}
