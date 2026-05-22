bbb2aef0ef9a2fb96c4df40f5699f70a72b09b33
berardinicolo26 <berardinicolo@gmail.com>
2026-05-22 12:21:40 +0200

    Update Main.java

--- src/com/game/main/Main.java
+++ src/com/game/main/Main.java
@@ -1,24 +1,21 @@
-/**
- * ENTRY POINT DELL'APPLICAZIONE
- * - Contiene solo il metodo public static void main(String[] args).
- * - Il suo unico scopo è istanziare la GameWindow e avviare il gioco.
- * - Non deve contenere logica di gioco.
- */
-
 package com.game.main;
 
 import com.game.core.GamePanel;
 
 public class Main {
     public static void main(String[] args) {
+        
+        // --- 1. ATTIVA L'ACCELERAZIONE HARDWARE (OpenGL) E IL V-SYNC ---
+        // Queste due righe dicono a Java di usare la Scheda Video invece del Processore
+        System.setProperty("sun.java2d.opengl", "true");
+        
+
         GamePanel gamePanel = new GamePanel(); 
 
         // Istanziamo la GameWindow. Le passiamo il gamePanel in modo che la finestra sappia cosa deve mostrare.
-        // Nota: non salviamo l'oggetto in una variabile (es. GameWindow gw = new...) 
-        // perché ci basta che venga creata e mostrata a schermo.
-
         new GameWindow(gamePanel); 
 
-        gamePanel.startGameThread(); // Controllare se in GamePanel è necessario chiamare questo metodo qui o se è già gestito internamente al costruttore.
+        // Avviamo il motore del gioco
+        gamePanel.startGameThread(); 
     }
 }
